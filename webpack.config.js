a294b7b95f172b2961ddaf2f10389e6d22f0ad1e
dicky dwijayanto <dicky.dwijayanto@gmail.com>
2017-04-14 21:09:54 +0700

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var assetsPath = path.join(__dirname, 'src');
 
 module.exports = {
     entry :  {
-        bundle :  ['webpack-dev-server/client?salty-gorge-71831.herokuapp.com',
+        bundle :  ['webpack-dev-server/client?https://dickydwijayanto-pokedex.herokuapp.com/',
             'webpack/hot/only-dev-server',
            path.resolve(assetsPath,'index.js')],
     },
@@ -12,7 +12,7 @@ module.exports = {
         chunkFilename: '[name].js',
         filename: '[name].js', //
         path: path.join(assetsPath ,"dist/js/"),
-        publicPath: 'http://salty-gorge-71831.herokuapp.com/assets/'
+        publicPath: 'https://dickydwijayanto-pokedex.herokuapp.com/assets/'
     },
     module: {
         loaders: [
